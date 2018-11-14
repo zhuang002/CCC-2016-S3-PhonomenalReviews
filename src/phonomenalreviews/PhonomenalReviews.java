600e5c1c822d67833b72fe9fb43dcd451d0ae679
zhuang002 <zhuang001@msn.com>
2018-11-14 18:44:04 -0500

    using start

--- src/phonomenalreviews/PhonomenalReviews.java
+++ src/phonomenalreviews/PhonomenalReviews.java
@@ -15,125 +15,165 @@ import java.util.Scanner;
  */
 public class PhonomenalReviews {
 
-    static HashMap<Integer, HashSet<Integer>> graph = new HashMap();
-    static HashSet<Integer> phoRestaurants = new HashSet();
-    static int N, M;
-    static int aPhoRestaurant = -1;
-    static HashMap<String,Integer> getTreeDepthBackup=new HashMap();
-
     /**
      * @param args the command line arguments
      */
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        Tree tree=readInTree(sc,N,M);
+        int diameter=tree.getDiameter();
+        int cutSize=tree.size();
+        System.out.println((cutSize - 1) * 2 - diameter);
+    }
+
+    private static Tree readInTree(Scanner sc, int N, int M) {
+        Tree tree=new Tree();
         
-        for (int i = 0; i < M; i++) {
-            aPhoRestaurant = sc.nextInt();
-            phoRestaurants.add(aPhoRestaurant);
+        for (int i=0;i<M;i++) {
+            tree.phoRestaurants.add(sc.nextInt());
         }
-        for (int i = 0; i < N - 1; i++) {
-            int node1 = sc.nextInt();
-            int node2 = sc.nextInt();
+        for (int i=0;i<N-1;i++) {
+            int id1=sc.nextInt();
+            int id2=sc.nextInt();
             
-            addConnection(node1, node2);
-            addConnection(node2, node1);
-
+            Node node1=tree.getNode(id1);
+            Node node2=tree.getNode(id2);
+            node1.addLink(node2);
+            node2.addLink(node1);
         }
-
-        removeUnnecessaryNodes(aPhoRestaurant);
-        int diameter = getTreeDiameter();
-        System.out.println((graph.size()-1)*2-diameter);
-    }
-
-    private static void addConnection(int node1, int node2) {
-        HashSet<Integer> connectedNodes;
-        if (graph.containsKey(node1)) {
-            connectedNodes = graph.get(node1);
-            connectedNodes.add(node2);
-        } else {
-            connectedNodes = new HashSet();
-            connectedNodes.add(node2);
-            graph.put(node1, connectedNodes);
-        }
-    }
-
-    private static void removeUnnecessaryNodes(int root) {
-        removeUnecessaryChildTree(null, root);
-    }
-
-    private static boolean removeUnecessaryChildTree(Integer root, Integer subRoot) {
-        HashSet<Integer> subNodes = graph.get(subRoot);
-        if (subNodes == null) {
-            return true;
-        }
-        HashSet<Integer> toBeRemoved=new HashSet();
-        for (Integer subNode : subNodes) {
-            if (subNode==root) continue;
-            
-            if (removeUnecessaryChildTree(subRoot, subNode)) {
-                toBeRemoved.add(subNode);
-            }
-        }
-        for (Integer subNode:toBeRemoved) {
-            removeSubtree(subNode);
-        }
-
-        if (subNodes.size() == 1 && !phoRestaurants.contains(subRoot)) {
-            //removeSubtree(subRoot);
-            return true;
-        }
-        return false;
-    }    
-
-    private static void removeSubtree(Integer subRoot) {
-        HashSet<Integer> connectedNodes=graph.get(subRoot);
-        for (Integer node : connectedNodes) {
-            graph.get(node).remove(subRoot); 
-        }
-        graph.remove(subRoot);
-    }
-
-    private static int getTreeDiameter() {
-        return getTreeDiameter(null,aPhoRestaurant);
-        
-    }
-
-    private static int getTreeDiameter(Object root, int subRoot) {
-        HashSet<Integer> connectedNodes=graph.get(subRoot);
-        int maxDiameter=-1;
-        int maxDepth1=-1;
-        int maxDepth2=-1;
-        for (Integer node:connectedNodes) {
-            if (node==root) continue;
-            int diameter=getTreeDiameter(subRoot,node);
-            if (diameter>maxDiameter) maxDiameter=diameter;
-            int depth=getTreeDepth(subRoot,node);
-            if (depth>=maxDepth1) {
-                maxDepth2=maxDepth1;
-                maxDepth1=depth;
-            } else if (depth>maxDepth2)
-                maxDepth2=depth;
-        }
-        return Math.max(maxDiameter, maxDepth1+maxDepth2+2);
-    }
-
-    private static int getTreeDepth(Integer root, Integer subRoot) {
-        String backupKey=root+"-"+subRoot;
-        if (getTreeDepthBackup.containsKey(backupKey)) return getTreeDepthBackup.get(backupKey);
-        
-        int maxDepth=-1;
-        HashSet<Integer> connectedNodes=graph.get(subRoot);
-        for (Integer node:connectedNodes) {
-            if (node==root) continue;
-            int depth=getTreeDepth(subRoot,node);
-            if (depth>maxDepth) maxDepth=depth;
-        }
-        maxDepth++;
-        getTreeDepthBackup.put(backupKey, maxDepth);
-        return maxDepth;
+        return tree;
     }
 
 }
+
+class Tree {
+    HashMap<Integer,Node> idMap=new HashMap();
+    HashSet<Integer> phoRestaurants=new HashSet();
+    Node aPho=null;
+    
+
+    int size() {
+        return this.idMap.size();
+    }
+
+    int getDiameter() {
+        Node startNode=getRandomPhoNode();
+        Node middleNode=getFurthestNode(startNode);
+        Node endNode=getFurthestNode(middleNode);
+        return endNode.info.depth;
+    }
+
+    private Node getRandomPhoNode() {
+        return this.aPho;
+    }
+
+    private Node getFurthestNode(Node root) {
+        root.info.depth=0;
+        //HashSet<Integer> unvisitedPho=(HashSet<Integer>)this.phoRestaurants.clone();
+        //if (root.isPho) unvisitedPho.remove(root.id);
+        return getFurthestNode(null,root/*,unvisitedPho*/);
+    }
+
+    private Node getFurthestNode(Node parent, Node root/*, HashSet<Integer> unvisitedPho*/) {
+        
+        if (isLeaf(parent,root/*,unvisitedPho*/)) {
+            if (!root.isPho) return null;
+            if (parent!=null) {
+                root.info.depth=parent.info.depth+1;
+            } else {
+                root.info.depth=0;
+            }
+            return root;
+        }
+            
+        Node furthestNode=null;
+        if (root.isPho) furthestNode=root;
+        HashSet<Node> tobeRemoved=new HashSet();
+        for (Node node:root.connectedNodes) {
+            if (node==parent) continue;
+            node.info.depth=root.info.depth+1;
+            
+            Node tmpFurthestNode=getFurthestNode(root,node/*,unvisitedPho*/);
+            
+            if (tmpFurthestNode==null) {
+                tobeRemoved.add(node);
+            } else {
+                if (furthestNode==null) {
+                    furthestNode=tmpFurthestNode;
+                } else {
+                       if (tmpFurthestNode.info.depth>furthestNode.info.depth)
+                           furthestNode=tmpFurthestNode;
+                }
+            }
+        }
+        for (Node node : tobeRemoved) {
+            remove(root,node);
+        }
+        return furthestNode;
+    }
+
+    private boolean isLeaf(Node parent, Node root/*, HashSet<Integer> unvisitedPho*/) {
+        if (parent==null) {
+            if (root.connectedNodes.isEmpty())
+                return true;
+            else return false;
+        }
+        if (root.connectedNodes.size()==1) return true;
+        /*if (root.isPho) {
+            unvisitedPho.remove(root.id);
+        } 
+        if (unvisitedPho.isEmpty())
+            return true;*/
+        return false;
+    }
+
+    Node getNode(int id) {
+        if (this.idMap.containsKey(id)) return this.idMap.get(id);
+        
+        Node node=new Node(id);
+        if (this.phoRestaurants.contains(id))
+        {
+            node.isPho=true;
+            if (this.aPho==null) this.aPho=node;
+        }
+        this.idMap.put(id, node);
+        
+        return node;
+    }
+
+    private void remove(Node parent,Node node) {
+        parent.connectedNodes.remove(node);
+        idMap.remove(node.id);
+    }
+    
+        
+}
+
+class Node {
+    int id=-1;
+    HashSet<Node> connectedNodes=new HashSet();
+    NodeInfo info=new NodeInfo();
+    boolean isPho=false;
+
+    Node(int pId) {
+        this.id=pId;
+    }
+
+    void addLink(Node node) {
+        this.connectedNodes.add(node);
+    }
+}
+
+class NodeInfo {
+    int depth=-1;
+    boolean tobeRemoved=false;
+}
+
+
+    
